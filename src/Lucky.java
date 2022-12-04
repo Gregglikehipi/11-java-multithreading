69863b6d24b9dfde92db5fb97c12c4d75bca961a
Gregg <arkhipov.zena@gmail.com>
2022-12-04 20:49:30 +0500

    UWU

--- src/Lucky.java
+++ src/Lucky.java
@@ -1,22 +1,24 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lucky {
-    static volatile int x = 0;
-    static volatile int count = 0;
+    static AtomicInteger x1 = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            synchronized (LuckyThread.class) {
+            int x = 0;
                 while (x < 999999) {
-                    x++;
+                    x = x1.getAndIncrement();
                     if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
                             % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
                         System.out.println(x);
-                        count++;
+                        count.getAndIncrement();
                     }
                 }
             }
         }
-    }
+
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new LuckyThread();
